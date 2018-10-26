c36d06bb37f5510edba5fbc679166276579eafb8
abraca-dabra <31224133+abraca-dabra@users.noreply.github.com>
2018-10-26 16:12:36 -0700

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 void mainMenu();
 void gameMenu();
 void checkPassword(string psw);
-void checkUsername(string name );
+void checkUsername(string name);
 
 int main()
 {
@@ -28,16 +29,16 @@ void mainMenu(){
     cout << endl;
     cout << endl;
     cout << "Enter your choice: ";
-    cout << endl;
     int choice;
     cin >> choice;
     //check choice is legal or not
-    while (choice != 1 or choice != 2 or choice != 3){
+    while (choice != 1 && choice != 2 && choice != 3){
         cout << "Your choice is not valid!" << endl;
         cout << "Please make a valid choice: ";
         cin >> choice;
     }
     string name_login, name, psd, psd1, psd2;
+    
     switch(choice){
         case 1:
             //LoginMenu
@@ -45,42 +46,56 @@ void mainMenu(){
             cout << "\t\tEnter your user name: " << endl;
             cin >> name_login;
             //checkUsername(name_login);
-
+            
             cout << "\t\tEnter your password: " << endl;
             cin >> psd;
             checkPassword(psd);
-
+            
             gameMenu();
-
+            
             break;
         case 2:
             //registerMenu
             //set username
             //tring name, password1, password2;
             cout << "*****************************************" << endl;
-            cout << "\t\tSet your user name: " << endl;
             cout << "\t\t-User Name should be combination of less than 8 digits or letters";
+            cout << endl;
+            cout << "\t\tSet your user name: ";
+            
             cin >> name;
             /*****************************************************/
             //check if the username is used by the other user or not
             /*****************************************************/
             //checkUsername(name);
-
+            
             //set password
-            do {
-                cout << "\t\tSet your password: " << endl;
-                cout << "\t\t-Password should be combination of 8 digits or letters";
+            cout << endl;
+            cout << "\t\t-Password should be combination of 8 digits or letters";
+            cout << endl;
+            
+            cout << "\t\tSet your password: ";
+            cin >> psd1;
+            checkUsername(name);
+            cout << "\t\t-Repeat your password:";
+            cin >> psd2;
+            
+            while (psd1 != psd2) {
+                cout << endl;
+                cout << "\t\tDifferent Password!" << endl;
+                
+                cout << "\t\tSet your password: ";
                 cin >> psd1;
                 checkUsername(name);
                 cout << "\t\t-Repeat your password:";
                 cin >> psd2;
-            } while (psd1 != psd2);
-
+            }
+            
             /*****************************************************/
             //save user's information
             /*****************************************************/
             gameMenu();
-
+            
             break;
         case 3:
             //Exit
@@ -102,32 +117,32 @@ void gameMenu(){
     cout << endl;
     cout << endl;
     cout << "Enter your choice: ";
-    cout << endl;
+
     int choice;
     cin >> choice;
-
+    
     //check choice is legal or not
-    while (choice != 1 or choice != 2 or choice != 3){
+    while (choice != 1 && choice != 2 && choice != 3 && choice != 4 && choice != 5){
         cout << "Your choice is not valid!" << endl;
         cout << "Please make a valid choice: ";
         cin >> choice;
     }
-
+    
     switch(choice){
         case 1:
             //Play Game
-
+            
             break;
         case 2:
             //Game Instructions
-
+            
             break;
         case 3:
             //High Score
             break;
         case 4:
             //Purchase
-
+            
             break;
         case 5:
             //Exit
