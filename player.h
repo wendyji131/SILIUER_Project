31c218d50ccb933f155cf559811fb4d6d069aee4
xiaomei ji <31259905+wendyji131@users.noreply.github.com>
2018-10-27 21:09:20 -0700

    Add files via upload

---
+++ player.h
@@ -0,0 +1,39 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#define TRUE 1
+#define FALSE 0
+
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class player{
+    //friend admin;
+public:
+    player();
+    player(string username, string psd);
+    ~player();
+
+    string getUsername();
+    string getPassword();
+    void changePassword();
+    string setPassword();
+    bool checkPassword(string& psw);
+    void printPlayerInfo();
+    void printScores();
+    int getHighestScore();
+
+private:
+    string username_;
+    string psd_;
+    char gender_;
+    string email_;
+    bool does_pay_;
+    string login_date_;
+    int highest_score_;
+    vector<int> scores_;
+};
+
+#endif
